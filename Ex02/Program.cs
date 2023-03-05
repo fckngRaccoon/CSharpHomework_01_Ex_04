78eca92905171ba07a46bc05addc2b7b11b8cc15
cppdev <fckngRaccoon@yandex.ru>
2023-03-05 23:37:48 +0300

    Ex02

---
+++ Ex02/Program.cs
@@ -0,0 +1,31 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.Readline();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetTrirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number<100)
+    {
+        Console.WriteLine("Треьтей цифры нет");
+        return false;
+    }
+}
+
+int number = Prompt("Введите число > ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetTrirdRank(number));
+}
